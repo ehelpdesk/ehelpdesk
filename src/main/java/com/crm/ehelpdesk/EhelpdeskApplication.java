00b93bf6dd65913aaac9df6efbb607156150500f
ehelpdesk <ehelpdesk1@gmail.com>
2019-12-25 16:08:57 +0530

    Initial Commit

---
+++ src/main/java/com/crm/ehelpdesk/EhelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.crm.ehelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhelpdeskApplication.class, args);
+	}
+
+}
